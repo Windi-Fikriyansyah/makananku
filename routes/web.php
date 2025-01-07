9a4703c8f5fa5aedf4bd2277b401285fb5ba9bc5
Cornelio <cornelioabdimash@gmail.com>
2025-01-07 11:14:27 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\MainController;
+
+Route::get('/', [MainController::class, 'dashboard'])->name('dashboard');
+Route::get('/cart', [MainController::class, 'cart'])->name('cart');
+Route::get('/payment', [MainController::class, 'payment'])->name('payment');
+Route::post('/payment/submit', [MainController::class, 'submitPayment'])->name('payment.submit');
+Route::get('/order-note', [MainController::class, 'orderNote'])->name('orderNote');
