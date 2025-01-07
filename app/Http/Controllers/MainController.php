629cbc58c9abcb7bc5ab8f087cd57a29240c5d96
Cornelio <cornelioabdimash@gmail.com>
2025-01-07 11:13:12 +0700

    Update MainController.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,4 +7,31 @@ use Illuminate\Http\Request;
 class MainController extends Controller
 {
     //
+    public function dashboard() {
+        return view('dashboard');
+    }
+    
+    public function cart() {
+        return view('cart');
+    }
+    
+    public function payment() {
+        return view('payment');
+    }
+    
+    public function submitPayment(Request $request) {
+        // Logika untuk memproses pembayaran (simulasi sementara)
+        $success = true; // Ganti dengan logika QRIS
+    
+        if ($success) {
+            return redirect()->route('orderNote')->with('message', 'Terimakasih sudah memesan, silahkan menunggu pesanan.');
+        } else {
+            return back()->with('error', 'Transaksi gagal, coba lagi.');
+        }
+    }
+    
+    public function orderNote() {
+        return view('orderNote');
+    }
+    
 }
